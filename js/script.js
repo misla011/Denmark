13f0567f6683d87da5853a17d4c96f8ca956067c
David <desantolima@gmail.com>
2025-04-22 10:09:04 -0300

    Adiciona efeito de hover nos cards
    Adiciona efeito de destaque (hover cinza) nos cards da seção "Favoritos dos Clientes". Melhora a interatividade visual da página.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.classList.add('card-hover');
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.classList.remove('card-hover');
+  });
+});
